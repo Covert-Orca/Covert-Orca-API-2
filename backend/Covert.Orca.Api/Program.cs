a9f0f2a69061291b9a16ccad8cfb23974ee4e635
tomjh704 <78053349+tomjh704@users.noreply.github.com>
2021-05-01 21:36:50 -0400

    Lab 5 Part 4

--- backend/Covert.Orca.Api/Program.cs
+++ backend/Covert.Orca.Api/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Covert.Orca.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,30 @@ namespace Covert.Orca.Api
 {
     public class Program
     {
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context, logger);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error occured creating the database.");
+                }
+            }
+        }
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
